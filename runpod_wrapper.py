fc5c81bb0d90c8189944cf9e002bea9bebaa053b
Pooya Haratian <pooya.haratian@teampicnic.com>
2023-11-22 16:00:57 +0100

    init

---
+++ runpod_wrapper.py
@@ -0,0 +1,36 @@
+import runpod
+from typing import Any, Literal, TypedDict
+import requests
+import sys
+
+
+class HandlerInput(TypedDict):
+    method_name: Literal["generate"]
+    input: Any
+
+
+class HandlerJob(TypedDict):
+    input: HandlerInput
+
+
+def handler(job: HandlerJob):
+    base_url = "http://0.0.0.0:11434"
+    input = job["input"]
+
+    # streaming is not supported in serverless mode
+    input["input"]["stream"] = False
+    print(sys.argv)
+    model = sys.argv[1]
+    input["input"]["model"] = model
+
+    response = requests.post(
+        url=f"{base_url}/api/{input['method_name']}/",
+        headers={"Content-Type": "application/json"},
+        json=input["input"],
+    )
+    response.encoding = "utf-8"
+
+    return response.json()
+
+
+runpod.serverless.start({"handler": handler})
