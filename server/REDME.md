cbb336fbc9fbd0479c0e30c3d8bf6ef87b61c529
Pooya Haratian <pooya.haratian@teampicnic.com>
2024-07-24 17:40:28 +0200

    Fix not stopping sub-process

--- server/REDME.md
+++
@@ -1,5 +0,0 @@
-# Server
-
-This folder is running on the server.
-The Dockerfile starts the Ollama server, then executes the `runpod_wrapper`.
-`runpod_wrapper` forwards the requests to the Ollama server.
--- server/start.sh
+++ server/start.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+cleanup() {
+    echo "Cleaning up..."
+    pkill -P $$ # Kill all child processes of the current script
+    exit 0
+}
+
+# Trap exit signals and call the cleanup function
+trap cleanup SIGINT SIGTERM
+
+# Kill any existing ollama processes
 pgrep ollama | xargs kill
 
+# Start the ollama server and log its output
 ollama serve 2>&1 | tee ollama.server.log &
-# Store the process ID (PID) of the background command
+OLLAMA_PID=$! # Store the process ID (PID) of the background command
 
 check_server_is_running() {
-    # Replace "Listening" with the actual expected output
     echo "Checking if server is running..."
     if cat ollama.server.log | grep -q "Listening"; then
         return 0 # Success
@@ -15,7 +25,7 @@ check_server_is_running() {
     fi
 }
 
-# Wait for the process to print "Listening"
+# Wait for the server to start
 while ! check_server_is_running; do
     sleep 5
 done
